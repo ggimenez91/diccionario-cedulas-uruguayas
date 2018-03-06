fa1a8983e62645920fc0fd69738617161ae20100
etb <a.gaston.gimenez@gmail.com>
2018-03-06 11:50:22 +0000

    Correccion calculo digito verificador y actualizacion de la lista

--- cedulas.py
+++ cedulas.py
@@ -1,10 +1,11 @@
-def digitoVerificador (numero):
+def digitoVerificador(numero):
     suma = 0
-    for x in range(0, 7):
-        suma += int(numero[x])
+    vec = [8, 1, 2, 3, 4, 7, 6]
+    for i in range(0, 7):
+        suma += int(numero[i]) * vec[i]
     return suma % 10
 
-def crearDiccionarioCedulas (nombre):
+def crearDiccionarioCedulas(nombre):
     try:
         cedulas = open(nombre, 'x')
     except FileExistsError:
